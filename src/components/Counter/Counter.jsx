aedad5bae47142fed31b020c8329b58a735dbbb8
pwei12 <wpwei_16@yahoo.com>
2019-02-21 11:19:35 +0800

    Part2: Add an increment button and a decrement button

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,6 +3,17 @@ import React, { Component } from 'react'
 export class Counter extends Component {
     state = { id: 1, value: 0 }
 
+    handleIncrement = () => {
+      const newValue = this.state.value + 1;
+      this.setState({value: newValue});
+      // this.setState({value: this.state.value + 1})
+    }
+    handleDecrement = () => {
+      const newValue = this.state.value - 1;
+      this.setState({ value: newValue });
+    }
+
+
   render() {
     return (
       <React.Fragment>
@@ -11,7 +22,8 @@ export class Counter extends Component {
             {this.state.value}
           </span>
         </h2>
-        
+        <button className= "btn btn-dark" onClick={this.handleIncrement}> + </button>
+        <button className="btn btn-dark" onClick={this.handleDecrement}> - </button>
       </React.Fragment>
 
     )
